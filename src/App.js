fd8aaeb8045b8462253c4a881c762a8466806ce2
JVicente960 <104857463+JVicente960@users.noreply.github.com>
2024-10-09 21:39:32 -0300

    Componentes adicionados, bandRequest, Home, NotificaBell e reportsPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesApp from './routes'
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,20 @@ import { Routes, Route} from 'react-router-dom'
 import Profile from '../pages/editProfile';
 import User from '../pages/User'
 import Report from '../pages/report'
+import Home from '../pages/home'
+import BanRequest from '../pages/banRequest'
+import ReportsPage from '../pages/reportsPage'
 
 
 function RoutesApp() {
     return(
         <Routes>
-            <Route path="/" element={ <User/>} />
+            <Route path="/" element={ <Home/>} />
+            <Route path="/user" element={ <User/>} />
             <Route path='/editProfile' element = {  <Profile/> }/>
             <Route path='/report' element = { <Report/> } />
-
+            <Route path='/banRequest' element = { <BanRequest/> } />
+            <Route path='/reportsPage' element = { <ReportsPage/> } />
         </Routes>
     )
 }
